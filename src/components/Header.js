e8a968799f118e247bcb7cc2919c26c7ad180a56
Marla Foreman <marla294@gmail.com>
2018-04-10 16:56:23 -0500

    Styling links

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,13 @@ class Header extends React.Component {
 	render() {
 		return (
 			<div className="header">
-				<h1>The spectacle before us was indeed sublime.</h1>
-				<h2>H2</h2>
-				<h3>H3</h3>
-				<h4>H4</h4>
-				<h5>H5</h5>
-				<h6>H6</h6>
-				<p>Paragraph</p>
+				<h1>H1 The spectacle before us was indeed sublime.</h1>
+				<h2>H2 The spectacle before us was indeed sublime.</h2>
+				<h3>H3 The spectacle before us was indeed sublime.</h3>
+				<h4>H4 The spectacle before us was indeed sublime.</h4>
+				<h5>H5 The spectacle before us was indeed sublime.</h5>
+				<h6>H6 The spectacle before us was indeed sublime.</h6>
+				<p>Paragraph The spectacle before us was indeed sublime.</p>
 				<a href="google.com">Link</a>
 			</div>
 		);
