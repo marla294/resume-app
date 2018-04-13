849ff6ce7c712e62bde5c17c43e478553149a353
Marla Foreman <marla294@gmail.com>
2018-04-13 12:06:00 -0500

    Tiny bit of styling on Projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,12 +5,7 @@ class Projects extends React.Component {
 	render() {
 		return (
 			<div>
-				<h1>Current Projects</h1>
-				<div className="row">
-					<Project />
-					<Project />
-					<Project />
-				</div>
+				<h1>Current Projects (2017-2018)</h1>
 				<div className="row">
 					<Project />
 				</div>
