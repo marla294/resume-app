b7f831ec9c285cad347e37ef1a6b8e82d85f0e3f
Marla Foreman <marla294@gmail.com>
2018-04-18 09:51:33 -0500

    Adding box shadow container to position; position styling

--- src/components/Position.js
+++ src/components/Position.js
@@ -16,25 +16,27 @@ class Position extends React.Component {
 		} = this.props.position;
 		return (
 			<a href={url} target="_blank">
-				<div className="position">
-					<div className="image flex-container justify-center">
-						<img src={image} alt={name} />
-					</div>
-					<div className="flex-container align-center">
-						<div className="info">
-							<div className="company">
-								<h2>{name}</h2>
-								<h3>{loc}</h3>
-								<p>
-									<i>{desc}</i>
-								</p>
-							</div>
-							<div className="my-title">
-								<h2>{title}</h2>
-								<h3>
-									{start} - {end}
-								</h3>
-								<p>{jobdesc}</p>
+				<div className="boxshadow-container">
+					<div className="position">
+						<div className="image flex-container justify-center">
+							<img src={image} alt={name} />
+						</div>
+						<div className="flex-container align-center">
+							<div className="info">
+								<div className="company">
+									<h2>{name}</h2>
+									<h3>{loc}</h3>
+									<p>
+										<i>{desc}</i>
+									</p>
+								</div>
+								<div className="my-title">
+									<h2>{title}</h2>
+									<h3>
+										{start} - {end}
+									</h3>
+									<p>{jobdesc}</p>
+								</div>
 							</div>
 						</div>
 					</div>
