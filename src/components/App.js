4764edc5fe28e69790939e58ada51fa238073c3f
Marla Foreman <marla294@gmail.com>
2018-04-30 12:05:33 -0500

    Adding a tiny bit to the contact page

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Projects from "./Projects";
 import PastWork from "./PastWork";
 import Education from "./Education";
+import Contact from "./Contact";
 import data from "../project-data";
 import positions from "../positions-data";
 import schools from "../school-data";
@@ -39,6 +40,7 @@ class App extends React.Component {
 				<Projects projects={this.state.projects} />
 				<PastWork positions={this.state.positions} />
 				<Education schools={this.state.schools} />
+				<Contact />
 			</div>
 		);
 	}
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,32 @@
 import React from "react";
+import "../css/ParentComponent.css";
 
 class Contact extends React.Component {
 	render() {
-		return;
+		return (
+			<div>
+				<div className="head">
+					<h1>Contact Me</h1>
+				</div>
+				<div>
+					<br />
+					<p>
+						If you would like to get in touch, please send me a
+						message on{" "}
+						<a
+							href="https://www.linkedin.com/in/marlaforeman/"
+							style={{ textDecoration: "underline" }}
+						>
+							LinkedIn
+						</a>
+					</p>
+					<br />
+					<br />
+					<br />
+					<br />
+				</div>
+			</div>
+		);
 	}
 }
 
